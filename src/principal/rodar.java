31a015b1650c5d8b28c5ac11e4827126ce775fa5
Tiago de Souza <tiagolindo7u7@gmail.com>
2024-11-29 23:14:18 -0300

    terminamos amem

---
+++ src/principal/rodar.java
@@ -0,0 +1,16 @@
+package principal;
+import GUI.view;
+
+import java.awt.EventQueue;
+import java.util.ArrayList;
+
+import GUI.view;
+
+public class rodar {
+	
+	public static void main(String[] args) {
+		view Programa = new view();
+		Programa.setVisible(true);
+	}
+
+}
